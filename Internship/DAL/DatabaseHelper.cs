868f82945cf39de9842e3a3be821a1049ed665d1
bibashmanjusubedi <acharabibash@outlook.com>
2025-04-02 21:48:01 +0545

    DAL(Data Access Layer) DatabaseHelper.cs for Database Connection

---
+++ Internship/DAL/DatabaseHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace Internship.DAL // DAL for Data Acess Layer
+{
+    public class DatabaseHelper
+    {
+        private static string connectionString = "my_connection_string";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+
+        }
+    }
+}
