3768bf84ee3fce15ea9937c6007bab8ddccfa7da
bibashmanjusubedi <acharabibash@outlook.com>
2025-04-03 15:33:56 +0545

    Database Connected

--- Internship/Controllers/HomeController.cs
+++ Internship/Controllers/HomeController.cs
@@ -1,20 +1,27 @@
 using System.Diagnostics;
 using Internship.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace Internship.Controllers
 {
     public class HomeController : Controller
     {
+        public IConfiguration Configuration { get; }
+       
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];//line 23
+            SqlConnection connection = new SqlConnection(connectionString);//line 24
             return View();
         }
 
@@ -28,5 +35,7 @@ namespace Internship.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
