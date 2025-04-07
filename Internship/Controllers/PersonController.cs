6688e41a20bef2ae1f955913743e052d28dba83b
bibashmanjusubedi <acharabibash@outlook.com>
2025-04-07 14:44:02 +0545

    Person Index View Created

---
+++ Internship/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Internship.DAL.Repositories;
+using Internship.Models;
+namespace Internship.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        private readonly PersonRepository _personRepository;
+
+        // Constructor to initialize the repository
+        public PersonController()
+        {
+            _personRepository = new PersonRepository();
+        }
+        public IActionResult Index()
+        {
+            List<Person> persons = _personRepository.GetAllPersons();
+            return View(persons);
+        }
+
+        //// GET: Asset/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ Internship/DAL/Repositories/PersonRepository.cs
@@ -0,0 +1,44 @@
+﻿using Internship.Models;
+using Internship.DAL;
+using Microsoft.Data.SqlClient;
+
+namespace Internship.DAL.Repositories
+{
+    public class PersonRepository
+    {
+        public List<Person> GetAllPersons()
+        {
+            List<Person> persons = new List<Person>();
+
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                string query = "SELECT * FROM Person";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Create a new AssetDetail object and map values from the SQL result
+                    var person = new Person
+                    {
+                        PId = (int)reader["PId"], 
+                        Name = reader["Name"]?.ToString(),
+                        Address = reader["Address"]?.ToString(),
+                        Phone = reader["Phone"]?.ToString(),
+                        LoginID = (int)reader["LoginID"],
+                        LoginStatus = reader["LoginStatus"] != DBNull.Value? bool.Parse(reader["LoginStatus"].ToString()): false,//line 30
+                        Password = reader["Password"]?.ToString(),
+                        Remarks = reader["remarks"]?.ToString()
+                    };
+
+                    persons.Add(person);
+                }
+            }
+
+
+            return persons;
+        }
+
+    }
+}
