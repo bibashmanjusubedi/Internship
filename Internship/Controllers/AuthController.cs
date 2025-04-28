12d7cfc8eb5cb7ec0336ba3032d584abc03e0b7b
bibashmanjusubedi <acharabibash@outlook.com>
2025-04-28 09:39:35 +0545

    AuthController.cs

---
+++ Internship/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Internship.DAL.Repositories;
+using Internship.Models;
+
+
+namespace Internship.Controllers
+{
+    [ApiController]
+    [Route("api/[controller")]
+    public class AuthController:ControllerBase
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly AuthRepository _authRepo;
+
+        public AuthController(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+            _authRepo = new AuthRepository();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if(_authRepo.ValidateUser(request.Username, request.Password))
+            {
+                var token = GenerateJwtToken(request.Username);
+                return Ok(token);
+            }
+
+            return Unauthorized("Invalid credentials");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name,username),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var creds = new SigningCredentials//todo
+        }
+
+       
+        
+    }
+}
---
+++ Internship/DAL/Repositories/AuthRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Internship.DAL.Repositories
+{
+    public class AuthRepository
+    {
+        public bool ValidateUser(string username,string password)
+        {
+            using(var connection = DatabaseHelper.GetConnection())
+            {
+                string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password=@Password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+        }
+    }
+}
